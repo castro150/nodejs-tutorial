52699fc5a13f3cfbf76c8804577d69d89c995f0b
castro150 <rafaelcarneiroget@hotmail.com>
2017-03-03 18:45:19 -0300

    Backend com login e registro

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('express-jwt');
+
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+var auth = jwt({ secret: 'SECRET', userProperty: 'payload' });
+// TODO é recomendável deixar o secret em uma variável de ambiente
+// (igual ao de Users.js), fora da base do código, porque é o segredo
+// usado para gerar os tokens. "userProperty" define qual propriedade
+// em "req" vai ser colocado os payload do token.
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,8 +28,9 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err, post) {
     if (err) { return next(err); }
@@ -50,7 +61,7 @@ router.get('/posts/:post', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post) {
     if (err) { return next(err); }
 
@@ -58,9 +69,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment) {
     if (err) { return next(err); }
@@ -87,7 +99,7 @@ router.param('comment', function(req, res, next, id) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment) {
     if (err) { return next(err); }
 
@@ -122,5 +134,39 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 //     });
 //   });
 // });
+
+router.post('/register', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields.' });
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    if (err) { return next(err); }
+
+    return res.json({ token: user.generateJWT() });
+  });
+});
+
+router.poset('/login', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields.' });
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+
+    if (user) {
+      return res.json({ token: user.generateJWT() });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
 
 module.exports = router;
