ad5d641a334298d16baf490e576995c6ca4c3eeb
castro150 <rafaelcarneiroget@hotmail.com>
2017-03-03 19:10:50 -0300

    Angular Server de auth

--- routes/index.js
+++ routes/index.js
@@ -153,7 +153,7 @@ router.post('/register', function(req, res, next) {
   });
 });
 
-router.poset('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
   if (!req.body.username || !req.body.password) {
     return res.status(400).json({ message: 'Please fill out all fields.' });
   }
